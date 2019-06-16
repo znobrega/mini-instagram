32e727e556bef33c27e92100de3b58e9c44f2c97
Carlos Nobrega <nobregacarlosjr@gmail.com>
2019-06-16 16:55:15 -0300

    frontend added

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import Routes from "./routes";
+
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes />
+    </BrowserRouter>
+  );
+};
+
+export default App;
